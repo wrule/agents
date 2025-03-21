eb5a2ee8ecb605b5fa091e2b8af47b9f54918aac
jimao <shihao.gu@perfma.com>
2025-03-21 19:14:24 +0800

    feat: toolExecute

--- src/mastra/utils/toolExecute.ts
+++ src/mastra/utils/toolExecute.ts
@@ -9,21 +9,19 @@ interface ToolExecuteResult {
 export
 type ToolExecuteFunction<T> = (t: T) => Promise<ToolExecuteResult>;
 
-const toolExecute = <T>(callback: ToolExecuteFunction<T>, name = '工具调用'): ToolExecuteFunction<T> => {
-  return async (t: T) => {
-    let result: ToolExecuteResult;
-    console.log(name, '->', t);
-    try {
-      result = await callback(t);
-    } catch (error: any) {
-      result = {
-        success: false,
-        prompt: `调用错误消息为：${error?.message ?? '未知错误'}\n请向用户解释，并引导用户正确操作`,
-      };
-    }
-    console.log(name, '<-', result);
-    return result;
-  };
+const toolExecute = async <T>(callback: ToolExecuteFunction<T>, t: T, name = '工具调用'): Promise<ToolExecuteResult> => {
+  let result: ToolExecuteResult;
+  console.log(name, '->', t);
+  try {
+    result = await callback(t);
+  } catch (error: any) {
+    result = {
+      success: false,
+      prompt: `调用错误消息为：${error?.message ?? '未知错误'}\n请向用户解释，并引导用户正确操作`,
+    };
+  }
+  console.log(name, '<-', result);
+  return result;
 }
 
 export default toolExecute;
