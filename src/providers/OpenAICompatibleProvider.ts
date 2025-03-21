d47a213b06df0abbe42389e70d973cb73261858b
jimao <shihao.gu@perfma.com>
2025-03-21 16:15:24 +0800

    feat: OpenAICompatibleProvider

--- src/providers/OpenAICompatibleProvider.ts
+++ src/providers/OpenAICompatibleProvider.ts
@@ -1,9 +1,10 @@
+import path from 'path';
 import { createOpenAICompatible } from '@ai-sdk/openai-compatible';
 
 const OpenAICompatibleProvider = createOpenAICompatible({
   name: 'provider',
   apiKey: process.env.OPENAI_API_KEY,
-  baseURL: process.env.BASE_URL + '/v1',
+  baseURL: path.join(process.env.BASE_URL!, '/v1'),
 });
 
 export default OpenAICompatibleProvider;
