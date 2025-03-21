d01338c1d72b2e4a861846e64b5b9cd2b381cde6
jimao <shihao.gu@perfma.com>
2025-03-21 19:36:03 +0800

    feat: outputSchema

--- src/mastra/utils/outputSchema.ts
+++ src/mastra/utils/outputSchema.ts
@@ -1,10 +1,12 @@
 import { z } from 'zod';
 
-const outputSchema = () => {
-  const result = {
-    success: z.boolean().describe('调用是否成功'),
-    prompt: z.string().optional().describe('向用户解释调用结果以及下一步操作的prompt'),
-  };
+const outputSchema = (schema: Record<string, any>) => {
+  const result: Record<string, any> = { };
+  Object.entries(schema).forEach(([key, value]) => {
+    result[key] = value.optional();
+  });
+  result.success = z.boolean().describe('调用是否成功');
+  result.prompt = z.string().optional().describe('向用户解释调用结果以及下一步操作的prompt');
   return result;
 };
 
