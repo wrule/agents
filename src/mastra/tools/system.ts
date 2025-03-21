315c513dabe9781cb12c9fdf81caec07678141c7
jimao <shihao.gu@perfma.com>
2025-03-21 20:01:00 +0800

    feat: fakeVoid

--- src/mastra/tools/system.ts
+++ src/mastra/tools/system.ts
@@ -3,15 +3,14 @@ import { z } from 'zod';
 import dayjs from 'dayjs';
 import toolExecute from '../utils/toolExecute';
 import { outputSchemaBase } from '../utils/outputSchema';
+import fakeVoid from '../utils/fakeVoid';
 
 export const getTimeNowTool = createTool({
   id: 'get-time-now',
   description: `
 当需要直接或间接获取当前时间，调用此工具
   `.trim(),
-  inputSchema: z.object({
-    a: z.number().min(0).max(1).describe('随机数，0或1'),
-  }),
+  inputSchema: fakeVoid,
   outputSchema: z.object({
     ...outputSchemaBase,
     time: z.string().describe('当前时间').optional(),
