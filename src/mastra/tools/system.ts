3e13b6b3f607c99168bf304cb0b8a969dfc9f392
jimao <shihao.gu@perfma.com>
2025-03-21 18:21:48 +0800

    feat: 调整时间工具

--- src/mastra/tools/system.ts
+++ src/mastra/tools/system.ts
@@ -8,16 +8,16 @@ export const timeNowTool = createTool({
 当需要直接或间接获取当前时间，调用此工具
   `.trim(),
   inputSchema: z.object({
-    a: z.number().min(0).max(1).describe('随机数，0或1'),
-    unit: z.enum([]).describe('偏移单位'),
+    offset: z.number().describe(`距离当前时间的偏移值`),
+    unit: z.enum([
+      'y', 'M', '', ''
+    ]).describe('偏移值的单位'),
   }),
   outputSchema: z.object({
     time: z.string().describe('当前时间'),
   }),
   execute: async ({ context }) => {
     const now = dayjs();
-    now.add(1, '')
-    now.subtract
     return {
       time: dayjs().format('YYYY-MM-DD HH:mm:ss.SSS dddd'),
     };
