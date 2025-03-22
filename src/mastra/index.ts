ed747fdca1cb1d29c872c7c5826cd93dea89d045
jimao <shihao.gu@perfma.com>
2025-03-22 14:28:12 +0800

    feat: 两个新的agent加入

--- src/mastra/index.ts
+++ src/mastra/index.ts
@@ -4,10 +4,12 @@ import { createLogger } from '@mastra/core/logger';
 import { weatherWorkflow } from './workflows';
 import { weatherAgent } from './agents';
 import OpenAICompatibleMiddleware from './middleware/OpenAICompatibleMiddleware';
+import { xsea_agent } from './agents/xsea_agent';
+import { touch_fish_agent } from './agents/touch_fish';
 
 export const mastra = new Mastra({
   workflows: { weatherWorkflow },
-  agents: { weatherAgent },
+  agents: { xsea_agent, touch_fish_agent, weatherAgent },
   logger: createLogger({
     name: 'Mastra',
     level: 'info',
