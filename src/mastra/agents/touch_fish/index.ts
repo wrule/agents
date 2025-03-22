ba27fa82d19f6015b0c6e8cba4afea78b99e3470
jimao <shihao.gu@perfma.com>
2025-03-22 14:29:26 +0800

    feat: 摸摸鱼

--- src/mastra/agents/touch_fish/index.ts
+++ src/mastra/agents/touch_fish/index.ts
@@ -3,7 +3,7 @@ import main from '../../../models/main';
 import { Memory } from '@mastra/memory';
 
 export const touch_fish_agent = new Agent({
-  name: 'touch_fish_agent',
+  name: '摸摸鱼',
   instructions: `
 与用户随便聊聊吧，你也可以与用户玩一些文字游戏，记得保持语言活泼生动
   `.trim(),
