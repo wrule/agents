a2f0e5c1f3529f5e92fe0609be25bbc3877ebaf8
jimao <shihao.gu@perfma.com>
2025-03-22 22:48:25 +0800

    feat: user agent

--- src/mastra/index.ts
+++ src/mastra/index.ts
@@ -4,10 +4,11 @@ import { createLogger } from '@mastra/core/logger';
 import { weatherWorkflow } from './workflows';
 import { weatherAgent } from './agents';
 import OpenAICompatibleMiddleware from './middleware/OpenAICompatibleMiddleware';
+import { user_agent } from './agents/user_agent';
 
 export const mastra = new Mastra({
   workflows: { weatherWorkflow },
-  agents: { weatherAgent },
+  agents: { weatherAgent, user_agent },
   logger: createLogger({
     name: 'Mastra',
     level: 'info',
