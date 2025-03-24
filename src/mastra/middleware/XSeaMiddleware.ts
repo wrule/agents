1abff68eaa580c259dcdbce4a9a27b3afe39babe
jimao <shihao.gu@perfma.com>
2025-03-24 17:45:31 +0800

    feat: xsea api

--- src/mastra/middleware/XSeaMiddleware.ts
+++ src/mastra/middleware/XSeaMiddleware.ts
@@ -8,7 +8,7 @@ type HonoMiddleware = (ctx: Context, next: Next) => any;
 const XSeaMiddleware: HonoMiddleware = async (ctx: Context, next: Next) => {
   try {
     const url = new URL(ctx.req.url);
-    const fullPath = (url.pathname + url.search).replace(/^\/v1\/chat\/completions\/xsea\/api/, '');
+    const fullPath = (url.pathname + url.search).replace(/^\/xsea\/api/, '');
     if (['GET'].includes(ctx.req.method)) {
       const res = await http.get(fullPath);
       return ctx.json(res.data, res.status as ContentfulStatusCode);
