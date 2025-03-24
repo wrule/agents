6ceb8c1919d99219b8d0f6f71726e2dad89abf50
jimao <shihao.gu@perfma.com>
2025-03-24 17:09:35 +0800

    feat: xsea中间件

--- src/mastra/index.ts
+++ src/mastra/index.ts
@@ -2,13 +2,13 @@ import { Mastra } from '@mastra/core/mastra';
 import { createLogger } from '@mastra/core/logger';
 import { weatherWorkflow } from './workflows';
 import OpenAICompatibleMiddleware from './middleware/OpenAICompatibleMiddleware';
+import XSeaMiddleware from './middleware/XSeaMiddleware';
 import { xsea_agent } from './agents/xsea_agent';
 import { touch_fish_agent } from './agents/touch_fish';
 import { jmeter_expert } from './agents/jmeter_expert';
 import { gatling_expert } from './agents/gatling_expert';
 import { shell_expert } from './agents/shell_expert';
 import { user_agent } from './agents/user_agent';
-import XSeaMiddleware from './middleware/XSea';
 
 export const mastra = new Mastra({
   workflows: { weatherWorkflow },
