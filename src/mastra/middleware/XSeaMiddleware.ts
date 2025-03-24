11845ec71ee0b4ea65411ea8f30aa20b274fcc42
jimao <shihao.gu@perfma.com>
2025-03-24 17:10:15 +0800

    feat: json

--- src/mastra/middleware/XSeaMiddleware.ts
+++ src/mastra/middleware/XSeaMiddleware.ts
@@ -6,7 +6,7 @@ type HonoMiddleware = (ctx: Context, next: Next) => any;
 const XSeaMiddleware: HonoMiddleware = async (ctx: Context, next: Next) => {
   try {
     const body = await ctx.req.json();
-    return body;
+    return ctx.json(body);
   } catch (error: any) {
     const code = 500;
     return ctx.json({ code, message: error.message ?? '未知错误' }, code);
