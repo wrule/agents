37dc276a0a144f4e2c070bc376d92974650c0fc9
jimao <shihao.gu@perfma.com>
2025-03-24 17:02:19 +0800

    feat: xsea中阿进件

---
+++ src/mastra/middleware/XSea.ts
@@ -0,0 +1,16 @@
+import { Context, Next } from 'hono';
+
+export
+type HonoMiddleware = (ctx: Context, next: Next) => any;
+
+const XSeaMiddleware: HonoMiddleware = async (ctx: Context, next: Next) => {
+  try {
+    const body = await ctx.req.json();
+    return body;
+  } catch (error: any) {
+    const code = 500;
+    return ctx.json({ code, message: error.message ?? '未知错误' }, code);
+  }
+}
+
+export default XSeaMiddleware;
