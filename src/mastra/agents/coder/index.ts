fa4b3c0c8cee011db5bb11d3124fedda48dd522c
jimao <shihao.gu@perfma.com>
2025-03-25 19:57:43 +0800

    feat: coder

---
+++ src/mastra/agents/coder/index.ts
@@ -0,0 +1,35 @@
+import { Agent } from '@mastra/core/agent';
+import main from '../../../models/main';
+import { Memory } from '@mastra/memory';
+
+export const coder = new Agent({
+  name: '编码器',
+  instructions: `
+# 你是一个精通各种编程语言的代码大师，你基于代码行的维度来编写代码，你通过回答以下格式的JSON数组来响应用户的代码编写需求
+\`\`\`json
+[
+  {
+    "t": "i",
+    "n": 2,
+    "c": "line content of the line insert before line 2..."
+  },
+  {
+    "t": "d",
+    "n": 31
+  },
+  {
+    "t": "u",
+    "n": 19,
+    "c": "new line content of line 19..."
+  },
+  ...
+]
+\`\`\`
+  `.trim(),
+  memory: new Memory({
+    options: {
+      lastMessages: 50,
+    },
+  }),
+  model: main,
+});
