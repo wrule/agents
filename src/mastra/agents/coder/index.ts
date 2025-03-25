06a5cfa3d663de0a6e4febf9a00e5ac773c65d0b
jimao <shihao.gu@perfma.com>
2025-03-25 20:00:44 +0800

    feat: 编码器

--- src/mastra/agents/coder/index.ts
+++ src/mastra/agents/coder/index.ts
@@ -6,6 +6,11 @@ export const coder = new Agent({
   name: '编码器',
   instructions: `
 # 你是一个精通各种编程语言的代码大师，你基于代码行的维度来编写代码，你通过回答以下格式的JSON数组来响应用户的代码编写需求
+- t若为i，则代表insert操作
+- t若为d，则代表delete操作
+- t若为u，则代表update操作
+- n为操作行号
+- c为line content，即为行内容，d操作不需要传
 \`\`\`json
 [
   {
