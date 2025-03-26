a8a9862349a2ad4784bff2cf289ede1d52e0b852
jimao <shihao.gu@perfma.com>
2025-03-26 17:29:57 +0800

    feat: 删除sys_token

--- src/mastra/agents/xsea_agent/api/http.ts
+++ src/mastra/agents/xsea_agent/api/http.ts
@@ -5,7 +5,7 @@ const http = axios.create({
   headers: {
     // AccessKey: "8508f519a2544277a4f17862e5950dfb",
     // AccessKeySecret: "6fe076d4dec0446784d03cd5e54a7d64",
-    cookie: 'sys_env_id=822313712173449216; sys_env_code=Init; sys_token=e335ab0f7b0642c5b43091c30e435f9e',
+    // cookie: 'sys_env_id=822313712173449216; sys_env_code=Init; sys_token=e335ab0f7b0642c5b43091c30e435f9e',
   },
 });
 
