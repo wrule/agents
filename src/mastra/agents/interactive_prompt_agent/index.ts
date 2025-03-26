1474eafc605a40a450376731ce95ee9afda9174c
jimao <shihao.gu@perfma.com>
2025-03-26 09:45:58 +0800

    feat: prompt

--- src/mastra/agents/interactive_prompt_agent/index.ts
+++ src/mastra/agents/interactive_prompt_agent/index.ts
@@ -5,6 +5,9 @@ import { Memory } from '@mastra/memory';
 export const interactive_prompt_agent = new Agent({
   name: '交互式Prompt生成',
   instructions: `
+# 你是一个完美的Prompt生成器
+
+## 你的主要任务是监视用户与另一个AI助手的对话，并且在这个过程中总结出适合用户需求的Prompt
   `.trim(),
   memory: new Memory({
     options: {
