0f04c63a850031faa92235f9b30f1af95bbc7547
jimao <shihao.gu@perfma.com>
2025-04-01 15:45:09 +0800

    feat: 调通的

--- test.sh
+++ test.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
-curl https://openrouter.ai/api/v1/chat/completions \
+echo $OPENROUTER_API_KEY
+curl https://dashscope.aliyuncs.com/compatible-mode/v1/chat/completions \
   -H "Content-Type: application/json" \
   -H "Authorization: Bearer $OPENROUTER_API_KEY" \
   -d '{
-  "model": "qwen/qwen2.5-32b-instruct",
+  "model": "qwen2.5-72b-instruct",
   "messages": [
     {
       "role": "user",
