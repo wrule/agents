6466d798d5b8b1202783e90de2417c88c68f6430
jimao <shihao.gu@perfma.com>
2025-04-01 12:34:56 +0800

    feat: xwind支持注入故障

--- src/mastra/agents/xwind_agent/index.ts
+++ src/mastra/agents/xwind_agent/index.ts
@@ -1,5 +1,7 @@
 import { Agent } from '@mastra/core/agent';
 import main from '../../../models/main';
+import { 注入故障工具 } from '../xsea_agent/tools/inject';
+import { Memory } from '@mastra/memory';
 
 export const xwind_agent = new Agent({
   name: 'XWind智能体',
@@ -24,6 +26,18 @@ export const xwind_agent = new Agent({
 - 避免长篇大论，如果有性能问题，请精准指出问题所在
 
 请根据提供的性能数据进行专业分析，提供准确的诊断和具体可行的建议。
+
+- 用户想注入故障或者异常的时候 -> 注入故障工具
+  - 避免询问多余信息
+  - 确保直接回答注入成功或者注入失败
   `.trim(),
+  memory: new Memory({
+    options: {
+      lastMessages: 50,
+    },
+  }),
   model: main,
+  tools: {
+    注入故障工具,
+  },
 });
