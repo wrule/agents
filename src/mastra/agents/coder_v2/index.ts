fab34d916cd7460973f83cb42873a77a8726aeb9
jimao <shihao.gu@perfma.com>
2025-04-01 20:39:06 +0800

    feat: 简写

--- src/mastra/agents/coder_v2/index.ts
+++ src/mastra/agents/coder_v2/index.ts
@@ -5,23 +5,23 @@ import { z } from 'zod';
 
 const parser = StructuredOutputParser.fromZodSchema(
   z.object({
-    introduction: z.string().max(200).describe('本次代码修改的介绍'),
-    actions: z.array(
+    i: z.string().max(200).describe('introduction，本次代码修改的介绍'),
+    a: z.array(
       z.object({
-        action: z.number().min(1).max(3).describe(`
+        t: z.number().min(1).max(3).describe(`
 操作类型序号
 1. 在第n行之前插入一个内容为c的新行
 2. 把第n行的内容修改为c
 3. 删第n行
         `.trim()),
-        line: z.number().min(1).describe('行号，代表操作第n行'),
-        content: z.string().optional().describe(`
+        n: z.number().min(1).describe('行号，代表操作第n行'),
+        c: z.string().optional().describe(`
 行内容
 - 如果操作类型为删除（t为3），则此字段不存在
         `.trim()),
       }),
     ),
-    summary: z.string().max(200).describe('本次代码修改的总结'),
+    s: z.string().max(200).describe('summary，本次代码修改的总结'),
   }),
 );
 
