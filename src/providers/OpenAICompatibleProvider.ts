2f97971a1cf5a2fe256bf91158fee560c8cd4088
jimao <shihao.gu@perfma.com>
2025-04-02 16:38:02 +0800

    feat: 错误的临时提交

--- src/providers/OpenAICompatibleProvider.ts
+++ src/providers/OpenAICompatibleProvider.ts
@@ -5,6 +5,12 @@ const OpenAICompatibleProvider = createOpenAICompatible({
   name: 'provider',
   apiKey: process.env.OPENAI_API_KEY,
   baseURL: path.join(process.env.BASE_URL!, '/v1'),
+  fetch: (...args) => {
+    const url = args[0];
+    const config = args[1];
+    console.log(url, config);
+    return fetch(...args);
+  },
 });
 
 export default OpenAICompatibleProvider;
