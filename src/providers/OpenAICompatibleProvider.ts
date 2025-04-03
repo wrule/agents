83f03da6ae86e092f968913ff257c3bc4ed23ba4
jimao <shihao.gu@perfma.com>
2025-04-03 12:53:49 +0800

    feat: 7.log

--- src/providers/OpenAICompatibleProvider.ts
+++ src/providers/OpenAICompatibleProvider.ts
@@ -30,6 +30,7 @@ const OpenAICompatibleProvider = createOpenAICompatible({
         while (true) {
           const { done, value } = await reader.read();
           if (done) break;
+          console.log(`[${new TextDecoder().decode(value)}]`);
           const lines = new TextDecoder().decode(value).split('\n').map((line) => line.trim()).filter((line) => line);
           const newLines: string[] = [];
           lines.forEach((line) => {
@@ -74,7 +75,7 @@ const OpenAICompatibleProvider = createOpenAICompatible({
             }
             newLines.push(line);
           });
-          console.log(newLines.join('\n\n'));
+          // console.log(newLines.join('\n\n'));
         }
       } catch (error) {
         console.error(error);
