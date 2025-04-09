092b63c50c46ed50c6aa1f412c6a19daec4fc279
jimao <shihao.gu@perfma.com>
2025-04-09 10:18:45 +0800

    feat: xchaos

---
+++ src/mastra/agents/xchaos_agent/api/http.ts
@@ -0,0 +1,17 @@
+import axios from 'axios';
+
+export
+const thttp = (cookie?: string) => {
+  const http = axios.create({
+    baseURL: `${process.env.XCHAOS}/api`,
+    headers: { cookie },
+  });
+  http.interceptors.response.use((response) => {
+    const { data } = response;
+    if (data.success === false) {
+      throw new Error(data.message ?? '未知原因');
+    }
+    return response;
+  });
+  return http;
+};
