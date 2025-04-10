c9cc8ce1e64161639ca607da041023d6b987bc3b
jimao <shihao.gu@perfma.com>
2025-04-10 10:38:09 +0800

    feat: xsky

--- src/mastra/agents/xsky_agent_v1/index.ts
+++ src/mastra/agents/xsky_agent_v1/index.ts
@@ -4,7 +4,8 @@ import main from '../../../models/main';
 export const xsky_agent_v1 = new Agent({
   name: 'XSky智能体V1',
   instructions: `
-
+XSky是一个统一监控服务平台
+你是XSky的AI小助手
   `.trim(),
   model: main,
 });
