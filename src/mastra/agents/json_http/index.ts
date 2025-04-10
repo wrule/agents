8db7ba8c1f42c19bacf4bae2a789ddeff4febc9e
jimao <shihao.gu@perfma.com>
2025-04-10 12:45:57 +0800

    feat: json结构

--- src/mastra/agents/json_http/index.ts
+++ src/mastra/agents/json_http/index.ts
@@ -1,32 +1,44 @@
 import { Agent } from '@mastra/core/agent';
 import main from '../../../models/main';
 import { z } from 'zod';
+import { StructuredOutputParser } from 'langchain/output_parsers';
+import { Memory } from '@mastra/memory';
 
-const httpRequest = z.object({
-  method: z.enum(['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'HEAD', 'OPTIONS', 'TRACE', 'CONNECT']),
-  protocol: z.enum(['HTTP', 'HTTPS']),
-  hostname: z.string(),
-  port: z.number().optional(),
-  pathname: z.string().optional(),
-  queries: z.array(
-    z.tuple([
-      z.string(),
-      z.string(),
-    ]),
-  ).optional(),
-  body: z.string().optional(),
-  headers: z.array(
-    z.tuple([
-      z.string(),
-      z.string(),
-    ]),
-  ).optional(),
-});
+const parser = StructuredOutputParser.fromZodSchema(
+  z.object({
+    method: z.enum(['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'HEAD', 'OPTIONS', 'TRACE', 'CONNECT']),
+    protocol: z.enum(['HTTP', 'HTTPS']),
+    hostname: z.string(),
+    port: z.number().optional(),
+    pathname: z.string().optional(),
+    queries: z.array(
+      z.tuple([
+        z.string(),
+        z.string(),
+      ]),
+    ).optional(),
+    body: z.string().optional(),
+    headers: z.array(
+      z.tuple([
+        z.string(),
+        z.string(),
+      ]),
+    ).optional(),
+  })
+);
 
 export const json_http = new Agent({
   name: 'JSON_HTTP',
   instructions: `
-你是HTTP
+# 你是HTTP Request的JSON结构生成器
+
+## 确保你的回答遵循以下JSON
+${parser.getFormatInstructions()}
   `.trim(),
+  memory: new Memory({
+    options: {
+      lastMessages: 50,
+    },
+  }),
   model: main,
 });
