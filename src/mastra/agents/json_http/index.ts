1ba39ac334b1e0e53af5770a4562a2860ab8f595
jimao <shihao.gu@perfma.com>
2025-04-10 12:24:30 +0800

    feat: 定义http请求结构

--- src/mastra/agents/json_http/index.ts
+++ src/mastra/agents/json_http/index.ts
@@ -1,5 +1,20 @@
 import { Agent } from '@mastra/core/agent';
 import main from '../../../models/main';
+import { z } from 'zod';
+
+const httpRequest = z.object({
+  method: z.enum(['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'HEAD', 'OPTIONS', 'TRACE', 'CONNECT']),
+  protocol: z.enum(['HTTP', 'HTTPS']),
+  hostname: z.string(),
+  port: z.number().optional(),
+  pathname: z.string().optional(),
+  headers: z.array(
+    z.tuple([
+      z.string(),
+      z.string(),
+    ]),
+  ).optional(),
+});
 
 export const json_http = new Agent({
   name: 'JSON_HTTP',
