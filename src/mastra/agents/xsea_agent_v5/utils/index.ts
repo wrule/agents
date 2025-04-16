b5a085ba1984eadec3c07afe92a0c3bb4680815e
jimao <shihao.gu@perfma.com>
2025-04-16 10:11:23 +0800

    fix: 不使用后端过滤

--- src/mastra/agents/xsea_agent_v5/utils/index.ts
+++ src/mastra/agents/xsea_agent_v5/utils/index.ts
@@ -31,7 +31,7 @@ export const XSeaNameField: { [key in XSeaType]: string } = {
 };
 
 export const exactSearch = async (query: string, type: XSeaType, cookie?: string) => {
-  const { data } = await thttp(cookie).post(`xsea/vector/query`, { type, text: query, topK: 2, filterScore: true, filterMap: { } });
+  const { data } = await thttp(cookie).post(`xsea/vector/query`, { type, text: query, topK: 2, filterScore: false, filterMap: { } });
   const list = data.object?.map((item: any) => ({
     ...item.data,
     score: item.score,
