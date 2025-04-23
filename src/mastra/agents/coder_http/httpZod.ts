6c136197ef7fe93470408bba7f1433bd172f7722
jimao <shihao.gu@perfma.com>
2025-04-23 12:56:06 +0800

    feat: 提交

--- src/mastra/agents/coder_http/httpZod.ts
+++ src/mastra/agents/coder_http/httpZod.ts
@@ -4,7 +4,7 @@ import { StructuredOutputParser } from 'langchain/output_parsers';
 const HttpZod = z.object({
   name: z.string().max(20).describe('行为，实体，修饰，等组成的准确中文名称').optional(),
   method: z.enum(['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'HEAD', 'OPTIONS']).optional(),
-  protocol: z.enum(['http', 'https']).optional(),
+  protocol: z.enum(['http://', 'https://']).optional(),
   hostname: z.string().optional(),
   port: z.number().optional(),
   pathname: z.string().optional(),
