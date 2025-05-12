8e5d6134e6fdf4d68d9a7be0d9c39930ef521e22
jimao <shihao.gu@perfma.com>
2025-05-12 18:39:58 +0800

    feat: errmessage

--- src/mastra/agents/xsea_agent/utils/index.ts
+++ src/mastra/agents/xsea_agent/utils/index.ts
@@ -92,7 +92,7 @@ interface ToolResult {
 export
 async function toolExecute<T>(name: string, t: T, callback: (t: T) => Promise<ToolResult>): Promise<ToolResult> {
   let result: ToolResult;
-  console.log(name, '->', t);
+  console.log(name, '->>', t);
   try {
     result = await callback(t);
   } catch (error: any) {
@@ -102,6 +102,6 @@ async function toolExecute<T>(name: string, t: T, callback: (t: T) => Promise<To
       prompt: `调用错误消息为：${error.message ?? '未知错误'}，请向用户解释，并引导用户正确操作`,
     };
   }
-  console.log(name, '<-', result);
+  console.log(name, '<<-', result);
   return result;
 }
