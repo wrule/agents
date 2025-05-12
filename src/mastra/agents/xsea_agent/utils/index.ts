52c19175ce809762f0e1770707fc1f6a02543df6
jimao <shihao.gu@perfma.com>
2025-05-12 18:37:17 +0800

    feat: errmessage

--- src/mastra/agents/xsea_agent/utils/index.ts
+++ src/mastra/agents/xsea_agent/utils/index.ts
@@ -102,6 +102,6 @@ async function toolExecute<T>(name: string, t: T, callback: (t: T) => Promise<To
       prompt: `调用错误消息为：${error.message ?? '未知错误'}，请向用户解释，并引导用户正确操作`,
     };
   }
-  console.log(name, '<-');
+  console.log(name, '<-', result);
   return result;
 }
